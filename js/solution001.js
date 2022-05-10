42a17025c1d4613bfc749d01bb2f970ccb453fdc
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-09 17:00:20 -0700

    Completed codewars repo setup

---
+++ js/solution001.js
@@ -0,0 +1,9 @@
+// Return the first Non Consecutive number from an array and return null if arr is consecutive
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  }
