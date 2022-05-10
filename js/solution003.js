565fb1273648d1e02df852a08bf0d0bb9d504574
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-10 12:37:29 -0700

    added solution and comment to solution003

--- js/solution003.js
+++ js/solution003.js
@@ -1,1 +1,9 @@
-console.log('new code')
+// Returns the sum of two lowest positive number in an arrray  
+
+function sumTwoSmallestNumbers(numbers) {  
+    let sortedArr = numbers.sort(function(a, b){
+      return a - b
+    })
+    
+    return sortedArr[0] + sortedArr[1]
+  }
