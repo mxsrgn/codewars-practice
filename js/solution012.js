b00e92030c5f4812cb5f9812394e96cf18950119
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-16 15:49:16 -0700

    Added solution012

---
+++ js/solution012.js
@@ -0,0 +1,9 @@
+// Function retruns ans array of the numbers between two given pararemeters inculuding parameters given.
+
+function between(a, b) {
+    let arr = []
+    for (let i = a; i <= b; i++){
+      arr.push(i)
+    }
+    return arr
+  }
