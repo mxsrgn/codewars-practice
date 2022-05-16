b475d8992a60d4cf72d81ac360e084eaf118e119
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-16 15:38:38 -0700

    Added solution011

---
+++ js/solution011.js
@@ -0,0 +1,7 @@
+// Return the volume of a cuboid with given parameters. Length, width, height.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height
+    }
+  }
