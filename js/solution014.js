5af00f6ab8034a2741308972f40c7300ed49ad01
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-18 12:19:06 -0700

    added solution014

---
+++ js/solution014.js
@@ -0,0 +1,13 @@
+//Function returns the suit of a given deck 
+
+function defineSuit(card) {
+    if (card.charAt(card.length -1) ==='♣'){
+      return 'clubs'
+    }else if (card.charAt(card.length -1) === '♦'){
+      return 'diamonds'
+    }else if (card.charAt(card.length -1) === '♥'){
+      return 'hearts'
+    }else if (card.charAt(card.length -1) === '♠'){
+      return 'spades'
+    }
+  }
