cd31770f1f2e0f90a827627f80c12adb91f0c82d
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-21 21:22:57 -0700

    Added solution016

---
+++ js/solution016.js
@@ -0,0 +1,7 @@
+// Simple function that return the total amount of goals scored in three seperate leagues
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+  
