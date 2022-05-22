076cd82abc8f7b412c1ffba12cbb64dd199dc258
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-22 10:22:59 -0700

    added solution019

---
+++ js/solution019.js
@@ -0,0 +1,7 @@
+// Function that takes 2 integers in the form of a stringanf outputs the sum in the form of a string
+
+function sumStr(a,b) {
+    let sumOfNums = Number(a) + Number(b)
+    return sumOfNums.toString()
+  }
+  
