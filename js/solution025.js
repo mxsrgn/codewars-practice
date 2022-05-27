852bfa4ddb43f41de4de0f86dede28ac321c4c24
Paolo Villagomez <mxsrgn@gmail.com>
2022-05-27 00:02:44 -0700

    made changes to solution025

--- js/solution025.js
+++ js/solution025.js
@@ -1,9 +1,10 @@
 // funtion returns a string with all numbers removed from string
 
 function stringClean(s){
-  let result = ''
-  for(let i=0;i<s.length;i++) {
-    if(isNaN(s[i])||s[i]==' ') result+=s[i]
+  let noNumberStr = ''
+  for (let i = 0; i < s.length; i++){
+    if(isNaN(s[i]) || s[i] == ' ')
+      noNumberStr += s[i]
     }
-  return result
+  return noNumberStr
   }
