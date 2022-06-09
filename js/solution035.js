fdd849d3cbff1a55932362dc35ea7c7f61ecabe8
Paolo Villagomez <mxsrgn@gmail.com>
2022-06-08 23:33:06 -0700

    added notes to solution035

--- js/solution035.js
+++ js/solution035.js
@@ -7,16 +7,20 @@ function titleCase(title, minorWords) {
       if(arguments.length === 1 || minorWords === undefined) {
         title = title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
       }else{
+    //minorWords Array 
         minorWords = minorWords.split(' ')
+    // convert title string to array
         title = title.split(' ')
-        let firstWord = title[0].charAt(0).toUppercase() + title[0].slice(1).toLowerCase()
+    // capatitalize the fist word
+        let firstWord = title[0].charAt(0).toUpperCase() + title[0].slice(1).toLowerCase()
+    // remove first word from array capitalize remaining words except minorWords
         title = title.slice(1).map(word => {
           for(let i = 0; i < minorWords.length; i++){
           if (word.toLowerCase() === minorWords[i].toLowerCase()){
               return word.toLowerCase()
               }
         }
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
     })
     title = firstWord + ' ' + title.join(' ')
     }
